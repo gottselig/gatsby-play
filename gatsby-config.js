92b800d207e2e9661e2e31e68e1a50c6bbf3e5e3
Gottselig Oleg <gottselig.oleg@gmail.com>
2020-12-15 10:08:05 +0000

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -5,6 +8,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
