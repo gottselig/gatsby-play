6b9932c1c3ad839c3e5c204b6b75381afa09246d
Gottselig Oleg <gottselig.oleg@gmail.com>
2020-12-15 11:01:03 +0000

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        environment: process.env.CONTENTFUL_ENVIRONMENT,
       },
     },
     `gatsby-plugin-react-helmet`,
